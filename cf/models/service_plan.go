1a45656c98f88b3ec0be59b9c4d53006938f2372
Anand Gaitonde <agaitonde@pivotallabs.com>
2014-07-17 11:57:28 -0700

    WiiiiiPPP

--- cf/models/service_plan.go
+++ cf/models/service_plan.go
@@ -1,8 +1,11 @@
 package models
 
 type ServicePlanFields struct {
-	Guid string
-	Name string
+	Guid   string
+	Name   string
+	Free   bool
+	Public bool
+	Active bool
 }
 
 type ServicePlan struct {
