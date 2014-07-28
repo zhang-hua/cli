d6830f76ae1d6636b3a7c5157137728ef6d22b6b
Dan Lavine, dlavine & Jonathan Berkhahn, jaberkha <pair+dan+jonathan@us.ibm.com>
2014-07-28 15:20:14 -0700

    WIP IT

--- cf/actors/services.go
+++ cf/actors/services.go
@@ -29,6 +29,8 @@ func NewServiceHandler(broker api.ServiceBrokerRepository, service api.ServiceRe
 	}
 }
 
+func 
+
 func (actor ServiceHandler) GetBrokerWithSingleService(serviceLabel string) ([]models.ServiceBroker, error) {
 	service, err := actor.serviceRepo.FindServiceOfferingByLabel(serviceLabel)
 	if err != nil {
