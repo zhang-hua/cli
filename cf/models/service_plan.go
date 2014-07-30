fde64671384d531f9bbfa7455de9e655f6204295
Dan Lavine, dlavine <pair+dan@us.ibm.com>
2014-07-30 09:30:09 -0700

    Wip it 3

--- cf/models/service_plan.go
+++ cf/models/service_plan.go
@@ -1,12 +1,13 @@
 package models
 
 type ServicePlanFields struct {
-	Guid     string
-	Name     string
-	Free     bool
-	Public   bool
-	Active   bool
-	OrgNames []string
+	Guid        string
+	Name        string
+	Free        bool
+	Description string
+	Public      bool
+	Active      bool
+	OrgNames    []string
 }
 
 type ServicePlan struct {
